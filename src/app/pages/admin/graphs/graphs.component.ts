f7dc81520e8f06b3d2884420545229e1405273e8
manu <manuelcorral97@gmail.com>
2021-06-17 23:17:51 +0200

    NOCHE 17 JUNIO Se avanza en las solicitud de fechas, obteniendo las fechas en formato string para preparar la petición al back.

--- src/app/pages/admin/graphs/graphs.component.ts
+++ src/app/pages/admin/graphs/graphs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-graphs',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphsComponent implements OnInit {
 
-  constructor() { }
+  maxTime: Date;
+  minTime: Date;
+  constructor() {
+    this.maxTime = new Date();
+    this.minTime = new Date();
+  }
 
   ngOnInit(): void {
   }
+
+  oneHour(){
+    console.log("Una hora");
+    var time = new Date();
+    const maxTime = formatDate(time, 'yyyy/MM/dd HH:mm:ss', 'en');
+    const minTime = formatDate(time.setHours(time.getHours()-1), 'yyyy/MM/dd HH:mm:ss', 'en');
+    this.peticion(maxTime,minTime);
+  }
+
+  oneDay(){
+    console.log("Un dia");
+    var time = new Date();
+    const maxTime = formatDate(time, 'yyyy/MM/dd HH:mm:ss', 'en');
+    const minTime = formatDate(time.setHours(time.getHours()-24), 'yyyy/MM/dd HH:mm:ss', 'en');
+    this.peticion(maxTime,minTime);
+  }
+
+  oneWeek(){
+    console.log("Una semana");
+    var time = new Date();
+    const maxTime = formatDate(time, 'yyyy/MM/dd HH:mm:ss', 'en');
+    const minTime = formatDate(time.setHours(time.getHours()-(24*7)), 'yyyy/MM/dd HH:mm:ss', 'en');
+    this.peticion(maxTime,minTime);
+  }
+
+  fechas(){
+    console.log("Rango de fechas");
+    var time = new Date();
+    const maxTime = ""
+    const minTime = ""
+    this.peticion(maxTime,minTime);
+  }
+
+  peticion(minTime: string, maxTime: string){
+    console.log(maxTime, minTime);
+
+  }
+
+
+  
 
 }
